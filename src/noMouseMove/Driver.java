52619eac095389f632fb3c0b2771bf1a9ef4ca74
Oleks Korshak <alexokorshak@gmail.com>
2018-11-15 15:20:32 -0700

    Start work on simplified/clean no mouse version

---
+++ src/noMouseMove/Driver.java
@@ -0,0 +1,13 @@
+package noMouseMove;
+
+public class Driver {
+	
+	public Driver() {
+		
+	}
+
+	public static void main(String[] args) {
+		new Driver();
+	}
+
+}
---
+++ src/noMouseMove/Settings.java
@@ -0,0 +1,29 @@
+package noMouseMove;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class Settings {
+
+	private int previousWindowXPosition;
+	private int previousWindowYPosition;
+	
+	private HashMap<String, String> valueMap;
+	
+	public Settings() {
+		valueMap = new HashMap<>();
+	}
+	
+	public void loadFromFile(File file) {
+		try(	FileReader fileReader = new FileReader(file);
+				BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+			
+		} catch (IOException e) {
+			System.err.println("Error loading settings from file " + file.getName());
+			e.printStackTrace();
+		}
+	}
+}
